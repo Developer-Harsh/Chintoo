19fb5fba44d0f8c20504c769e38f92a864ec8301
snevatechnologies <harsh95829@gmail.com>
2023-05-28 01:33:54 +0530

    edited some files

---
+++ app/src/main/java/com/harproj/harottapp/PaytmPaymentGatway.java
@@ -0,0 +1,15 @@
+package com.harproj.harottapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PaytmPaymentGatway extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paytm_payment_gatway);
+
+    }
+}
---
+++ app/src/main/java/com/harproj/harottapp/utils/App.java
@@ -0,0 +1,10 @@
+package com.harproj.harottapp.utils;
+
+import android.app.Application;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
